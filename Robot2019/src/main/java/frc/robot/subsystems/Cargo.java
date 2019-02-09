e79e5a91d6bce328a1bdbb1ddfdc8e23f7dadd13
David Fang <dfang314@gmail.com>
2019-02-09 14:18:43 -0800

    Move TODO statement to more relevant spot, formatted two documents

--- Robot2019/src/main/java/frc/robot/subsystems/Cargo.java
+++ Robot2019/src/main/java/frc/robot/subsystems/Cargo.java
@@ -17,7 +17,7 @@ public class Cargo extends Subsystem {
 
   private VictorSP roller;
   private PowerDistributionPanel pdp;
-  private int rollerPort;   // The port for the VictorSP on the PDP, not the RoboRIO.
+  private int rollerPort; // The port for the VictorSP on the PDP, not the RoboRIO.
 
   public Cargo(VictorSP roller, PowerDistributionPanel pdp, int rollerPort) {
     this.roller = roller;
@@ -39,7 +39,6 @@ public class Cargo extends Subsystem {
 
   public boolean hasCargo() {
     return pdp.getCurrent(rollerPort) > 15;
-    // TODO: set ports to actual cargo motor port
   }
 
   @Override
