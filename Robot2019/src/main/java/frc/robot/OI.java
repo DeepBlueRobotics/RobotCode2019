5c8bf359558ea166569b94c25e1d4ebd251d7e1f
David Fang <28679297+doawelul@users.noreply.github.com>
2019-02-17 16:07:08 -0800

    Address issue 37 by having the hatch stay out while intaking cargo (#41)
    * Address issue 37 by having the hatch stay out while intaking cargo
    
    * Make WaitMove only detect one-way movement
    
    * Fix incorrect subtraction logic

--- Robot2019/src/main/java/frc/robot/OI.java
+++ Robot2019/src/main/java/frc/robot/OI.java
@@ -11,18 +11,17 @@ import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoSink;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
-import frc.robot.commands.EjectCargo;
-import frc.robot.commands.IntakeCargo;
-import frc.robot.commands.SlowDrive;
-import frc.robot.commands.ToggleHatch;
-import frc.robot.commands.ToggleCamera;
 import frc.robot.commands.ActuateClimberRails;
 import frc.robot.commands.Climb;
-import frc.robot.subsystems.HatchPanel;
+import frc.robot.commands.EjectCargo;
+import frc.robot.commands.IntakeOnlyCargo;
+import frc.robot.commands.SlowDrive;
+import frc.robot.commands.ToggleCamera;
+import frc.robot.commands.ToggleHatch;
 import frc.robot.subsystems.Cargo;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.HatchPanel;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -53,7 +52,7 @@ public class OI {
     toggleHatchBtn.whenPressed(new ToggleHatch(hp));
 
     cargoIntakeBtn = new JoystickButton(manipulator, Manip.A); // TODO: set ports to correct values
-    cargoIntakeBtn.whenPressed(new IntakeCargo(cargo));
+    cargoIntakeBtn.whenPressed(new IntakeOnlyCargo(cargo, hp, dt));
     cargoEjectBtn = new JoystickButton(manipulator, Manip.B); // TODO: set ports to correct values
     cargoEjectBtn.whenPressed(new EjectCargo(cargo));
 
---
+++ Robot2019/src/main/java/frc/robot/commands/WaitMove.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Drivetrain.Side;
+
+public class WaitMove extends Command {
+  Drivetrain dt;
+  double startl;
+  double startr;
+
+  private final double reqDist = 2; // TODO: Test value
+
+  public WaitMove(Drivetrain dt) {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    this.dt = dt;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    startr = dt.getEncDist(Side.RIGHT);
+    startl = dt.getEncDist(Side.LEFT);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return startr - dt.getEncDist(Side.RIGHT) > reqDist && startl - dt.getEncDist(Side.LEFT) > reqDist;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
+  }
+}
