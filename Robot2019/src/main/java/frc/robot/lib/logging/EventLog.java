a9bcfeeb7584e61ccf396a9f796a9d057d8e8b27
Ryan DeBarger <806394@seq.org>
2019-12-22 12:28:39 -0800

    added event logging

--- Robot2019/src/main/java/frc/robot/lib/logging/EventLog.java
+++ Robot2019/src/main/java/frc/robot/lib/logging/EventLog.java
@@ -1,6 +1,7 @@
 package frc.robot.lib.logging;
 
 import java.io.IOException;
+import java.util.logging.ErrorManager;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -11,34 +12,68 @@ final class EventLog {
 
     private static Logger logger;
     private static Handler handler;
+    private static ErrorManager errorManager;
 
     static {
         logger = Logger.getLogger("RobotLogger");
         logger.setUseParentHandlers(false);
-        logger.setLevel(Level.ALL);
+        logger.setLevel(Level.INFO);
+        errorManager = new ErrorManager() {
+            @Override
+            public synchronized void error(String msg, Exception ex, int code) {
+                EventLog.error(msg, ex, code);
+            }
+        };
     }
 
-    void init() {
+    static void init() {
         if(!GlobalLogInfo.isInit()) {
             return;
         }
         try {
             handler = new FileHandler(GlobalLogInfo.getEventFile().getAbsolutePath(), false);
+            handler.setErrorManager(errorManager);
             System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tc \n\t %4$: %5$");
             handler.setFormatter(new SimpleFormatter());
             logger.addHandler(handler);
         } catch(IOException e) {
-            GlobalLogInfo.disableEvents();
-            System.err.println("Event logging setup failed robot will continue to run with event logging disabled");
+            LogUtils.handleLoggingError(true, "setting up event log handler", e);
         }
     }
 
-    void setLoggingLevel(Level level) {
+    static void setLoggingLevel(Level level) {
         logger.setLevel(level);
     }
 
-    void log(String message, Level level) {
+    static void log(String message, Level level) throws IllegalStateException {
+        LogUtils.checkInit();
         logger.log(level, message);
     }
+
+    private static synchronized void error(String msg, Exception ex, int code) {
+        String task;
+        switch(code) {
+            case ErrorManager.OPEN_FAILURE:
+                task = "while opening an output stream";
+                break;
+            case ErrorManager.CLOSE_FAILURE:
+                task = "while closing an output stream";
+                break;
+            case ErrorManager.FLUSH_FAILURE:
+                task = "while flushing an output stream";
+                break;
+            case ErrorManager.FORMAT_FAILURE:
+                task = "while formatting a message";
+                break;
+            case ErrorManager.GENERIC_FAILURE:
+            default:
+                task = "of type GENERIC_FAILURE";
+                break;
+        }
+        String message = task + (msg != null ? " with message " + msg : "");
+        LogUtils.handleLoggingError(true, message, ex);
+    }
+
+    private EventLog() {}
 
 }
--- Robot2019/src/main/java/frc/robot/lib/logging/Log.java
+++ Robot2019/src/main/java/frc/robot/lib/logging/Log.java
@@ -1,7 +1,33 @@
 package frc.robot.lib.logging;
 
+import java.util.logging.Level;
+
 public final class Log {
 
-    
+    public static void logErrorMessage(String message) throws IllegalStateException {
+        EventLog.log(message, Level.SEVERE);
+    }
+
+    public static void logWarningMessage(String message) throws IllegalStateException {
+        EventLog.log(message, Level.WARNING);
+    }
+
+    public static void logInfoMessage(String message) throws IllegalStateException {
+        EventLog.log(message, Level.INFO);
+    }
+
+    public static void logConfigMessage(String message) throws IllegalStateException {
+        EventLog.log(message, Level.CONFIG);
+    }
+
+    public static void setLoggingLevel(Level level) {
+        EventLog.setLoggingLevel(level);
+    }
+
+    public static void disableEventLogging() {
+        setLoggingLevel(Level.OFF);
+    }
+
+    private Log() {}
 
 }
