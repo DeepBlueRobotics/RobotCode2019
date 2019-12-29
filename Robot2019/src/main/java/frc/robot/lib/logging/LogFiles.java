b0867cb6b1376d7612b24f95f339952fcbf4c2f9
Ryan DeBarger <806394@seq.org>
2019-12-28 20:03:56 -0800

    removed unneccesary import

--- Robot2019/src/main/java/frc/robot/lib/logging/LogFiles.java
+++ Robot2019/src/main/java/frc/robot/lib/logging/LogFiles.java
@@ -3,7 +3,6 @@ package frc.robot.lib.logging;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
