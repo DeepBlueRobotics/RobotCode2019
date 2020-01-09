bca44fbf9c8a90dec9318f380fee0d75361e489c
Alexander McDowell <lextmcdowell@gmail.com>
2020-01-08 17:24:39 -0800

    Basic characterized drive functionality

--- Robot2019/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Robot2019/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,10 +7,6 @@
 
 package frc.robot.subsystems;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
@@ -34,13 +30,10 @@ public class Drivetrain extends Subsystem {
   private Encoder leftEnc, rightEnc;
   private AHRS ahrs;
 
+  private double kVolt, kVel, kAccel;
+  private double maxAccel;
+
   private double maxVoltage;
-
-  private double flKV, flKA, flVI;
-  private double frKV, frKA, frVI;
-  private double blKV, blKA, blVI;
-  private double brKV, brKA, brVI;
-
   private boolean wobbleDone;
 
   public Drivetrain(WPI_TalonSRX leftMaster, BaseMotorController leftSlave1, BaseMotorController leftSlave2,
@@ -71,7 +64,12 @@ public class Drivetrain extends Subsystem {
     rightEnc.setReverseDirection(true);
 
     this.ahrs = ahrs;
-    updateDrivetrainParameters();
+
+    // TODO: Update the actual values
+    kVolt = 0.0;
+    kVel = 0.0;
+    kAccel = 0.0;
+    maxAccel = 0.0;
 
     maxVoltage = 12.0;
     wobbleDone = false;
@@ -168,92 +166,22 @@ public class Drivetrain extends Subsystem {
     rightMaster.enableVoltageCompensation(false);
   }
 
-  public void updateDrivetrainParameters() {
-    try {
-      Scanner filereader = new Scanner(new File("/home/lvuser/drive_char_params.csv"));
-      String line = filereader.next();
-      // Forward-Left
-      flKV = Double.valueOf(line.split(",")[0]);
-      flKA = Double.valueOf(line.split(",")[1]);
-      flVI = Double.valueOf(line.split(",")[2]);
-      System.out.println(flKV + "," + flKA + "," + flVI);
+  // Characterizes the left/right motor values.
+  // Left and right are both values from -1 to 1.
+  public double[] characterizedDrive(double left, double right) {
+    double dt = 0.02;   // Characterized Drive is called 50 times a second
 
-      line = filereader.next();
-      // Forward-right
-      frKV = Double.valueOf(line.split(",")[0]);
-      frKA = Double.valueOf(line.split(",")[1]);
-      frVI = Double.valueOf(line.split(",")[2]);
-      System.out.println(frKV + "," + frKA + "," + frVI);
+    double actualLeftVel = getEncRate(Side.LEFT);
+    double actualRightVel = getEncRate(Side.RIGHT);
 
-      line = filereader.next();
-      // Backward-Left
-      blKV = Double.valueOf(line.split(",")[0]);
-      blKA = Double.valueOf(line.split(",")[1]);
-      blVI = Double.valueOf(line.split(",")[2]);
-      System.out.println(blKV + "," + blKA + "," + blVI);
+    double desiredLeftVel = this.getMaxSpeed() * left;
+    double desiredRightVel = this.getMaxSpeed() * right;
 
-      line = filereader.next();
-      // Backward-Right
-      brKV = Double.valueOf(line.split(",")[0]);
-      brKA = Double.valueOf(line.split(",")[1]);
-      brVI = Double.valueOf(line.split(",")[2]);
-      System.out.println(brKV + "," + brKA + "," + brVI);
-      filereader.close();
+    double leftAccel = (desiredLeftVel - actualLeftVel) / dt;
+    double rightAccel = (desiredRightVel - actualRightVel) / dt;
 
-      // Averaging numbers because they vary so much
-      double avg = (flKA + frKA + blKA + brKA) / 4;
-      flKA = avg;
-      frKA = avg;
-      blKA = avg;
-      brKA = avg;
-    } catch (FileNotFoundException e) {
-      flKV = 0.06369046755507658;
-      flKA = 0.0215894793277297;
-      flVI = 0.8403701236277824;
-
-      frKV = 0.0619423013628032;
-      frKA = 0.04044703465602449;
-      frVI = 0.810212379284332;
-
-      blKV = 0.06388520699977113;
-      blKA = 0.025492804438184545;
-      blVI = 0.8071078220643216;
-
-      brKV = 0.06140765089854154;
-      brKA = 0.042046502553651215;
-      brVI = 0.7929289166816246;
-      
-      e.printStackTrace();
-    }
-  }
-
-  public double calculateVoltage(Direction dir, double velocity, double acceleration) {
-    /*System.out.println("Velocity: " + velocity + ", Acceleration: " + acceleration);
-    System.out.println(flKV + "," + flKA + "," + flVI);
-    System.out.println(frKV + "," + frKA + "," + frVI);
-    System.out.println(blKV + "," + blKA + "," + blVI);
-    System.out.println(brKV + "," + brKA + "," + brVI);*/
-
-    if (dir == Direction.FL) {
-      return flKV * velocity + flKA * acceleration + flVI;
-    } else if (dir == Direction.FR) {
-      return frKV * velocity + frKA * acceleration + frVI;
-    } else if (dir == Direction.BL) {
-      return blKV * velocity + blKA * acceleration - blVI;
-    } else {
-      return brKV * velocity + brKA * acceleration - brVI;
-    }
-  }
-
-  public double calculateAcceleration(Direction dir, double velocity, double voltage) {
-    if (dir == Direction.FL) {
-      return (voltage - flKV * velocity - flVI) / flKA;
-    } else if (dir == Direction.FR) {
-      return (voltage - frKV * velocity - frVI) / frKA;
-    } else if (dir == Direction.BL) {
-      return (voltage - blKV * velocity - blVI) / blKA;
-    } else {
-      return (voltage - brKV * velocity + brVI) / brKA;
-    }
+    double newLeft = kVolt + kVel * actualLeftVel + kAccel * leftAccel;
+    double newRight = kVolt + kVel * actualRightVel + kAccel * rightAccel;
+    return new double[]{newLeft, newRight};
   }
 }
